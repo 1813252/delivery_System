1607c328e08cd02f65a2c4c56ca55c42a1774e40
1813252 <55134827+1813252@users.noreply.github.com>
2019-12-12 20:14:52 +0900

    Update stor.c

--- stor.c
+++ stor.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_MSG_SIZE			10
+#define MAX_MSG_SIZE			100
 #define PASSWD_LEN				4
 #define N_BUILDING				10
 #define N_FLOOR					10
@@ -20,23 +20,25 @@ typedef struct {
 
 static storage_t** deliverySystem; 	
 static int storedCnt =0;
+static int systemSize[2] = {0, 0};  
+static char masterPassword[PASSWD_LEN+1];
 
 void str_printStorageStatus(void) {
 	
 	int i, j;
-	printf("----------------------------- Delivery Storage System Status (%i occupied out of %i )-----------------------------\n\n", storedCnt,24);
+	printf("----------------------------- Delivery Storage System Status (%i occupied out of %i )-----------------------------\n\n", storedCnt,systemSize[0]*systemSize[1]);
 	
 	printf("\t");
-	for (j=0;j<6;j++)
+	for (j=0;j<systemSize[1];j++)
 	{
 		printf(" %i\t\t",j);
 	}
 	printf("\n-----------------------------------------------------------------------------------------------------------------\n");
 	
-	for (i=0;i<4;i++)
+	for (i=0;i<systemSize[0];i++)
 	{
 		printf("%i|\t",i);
-		for (j=0;j<6;j++)
+		for (j=0;j<systemSize[1];j++)
 		{
 			if (deliverySystem[i][j].cnt > 0)
 			{
@@ -58,53 +60,67 @@ int str_createSystem(char *filepath){
 	
 	int i,j=0;
  	
-	deliverySystem = (storage_t**)malloc(sizeof(storage_t*)*4); 
+ 	fp= fopen(filepath,"r");	//파일 오픈  
+			
+		
+		int line=0; //텍스트 파일 몇줄인지 세보기  
+		char c;      
+		
+		if (fp==NULL)
+		{
+			return -1; //파일이 안 열리면 거기서 끝  
+			
+		}
+		
+		while ((c=fgetc(fp)) !=EOF) // 파일  끝까지 읽기  
+		{
+			if (c=='\n') //줄 바뀔 때 마다  
+			
+				line++; //line ++ 
+		}
+			
+		fclose(fp); //다 읽었으니 닫아줌  
+		
+		fp= fopen(filepath,"r");	 //다시 열어요  
+		
+		fscanf(fp, "%d %d %s", &systemSize[0], &systemSize[1], masterPassword); // 행,열,마스터 비밀번호 받기  
+ 	
+ 	
+		deliverySystem = (storage_t**)malloc(sizeof(storage_t*)*systemSize[0]);  //이중포인터 deliverySystem 이 deliverySystem[i] 를 가르키도록 함// 
 
-		for(i=0;i<4;i++)
+		for(i=0;i<systemSize[0];i++)
 		{
 			
-			deliverySystem[i] = (storage_t*)malloc(sizeof(storage_t)*6); 
+			deliverySystem[i] = (storage_t*)malloc(sizeof(storage_t)*systemSize[1]); //deliverySystem[i]는 deliverySystem[i][0]을 가르킵니다//
 		
 		}
 			
-		fp= fopen(filepath,"r");		
-		
-		int line=0;
-		char c;
-		
-		while ((c=fgetc(fp)) !=EOF)
-		{
-			if (c=='\n')
-			
-				line++;
-		}
-			
-		fclose(fp);
-		
-		fp= fopen(filepath,"r");		
 	
 		for(i=0;i<4;i++)
 		{
 			for(j=0;j<6;j++)
-			deliverySystem[i][j].cnt=0;
+			deliverySystem[i][j].cnt=0; //deliverySystem[i][j].cnt=0 으로 초기화하여 화면에 trash 값을 띄우지 않도록 합니다//
 		}
 		
-		int k;
+		
+		int k; 
 			
-		for(k=0;k<line;k++)
+		for(k=2;k<line;k++) //3번째 줄부터 마지막 줄까지 읽습니다
 		{
-			fscanf(fp,"%i %i",&i ,&j);
-			deliverySystem[i][j].context= (char*)malloc(sizeof(char)*100);
+			fscanf(fp,"%i %i",&i ,&j); //앞에 두 정수는 i, j 값에 저장합니다
+			deliverySystem[i][j].context= (char*)malloc(sizeof(char)*100); //일단 100글자로 메모리 할당을 해줍니다//
 			fscanf(fp," %i %i %s %s", &deliverySystem[i][j].building, &deliverySystem[i][j].room, deliverySystem[i][j].passwd, deliverySystem[i][j].context);
-			deliverySystem[i][j].cnt++;
-			storedCnt++;
+			// passwd와 context는 각각 배열, 포인터 이므로 주소연산자 &를 사용하지 않습니다//
+			
+			deliverySystem[i][j].cnt++; //cnt=1을 만들어주어 프로그램 화면에 띄웁니다//
+			storedCnt++;//저장된 택배수가 ++ 됩니다.
 		}
 		
 		
-		fclose(fp);
+		fclose(fp); //파일을 닫습니다.
 
 		
-	return 0;
+	return 0; //시스템을 잘 만들었으면 0을 반환합니다 
 	
 }
 
@@ -118,9 +134,9 @@ int str_createSystem(char *filepath){
 int str_pushToStorage(int x, int y, int nBuilding, int nRoom, char msg[MAX_MSG_SIZE+1], char passwd[PASSWD_LEN+1]) {
 	
 
-	deliverySystem[x][y].context= (char*)malloc(sizeof(char)*strlen(msg));
+	deliverySystem[x][y].context= (char*)malloc(sizeof(char)*strlen(msg)); //받은 message만큼 context 포인터에 메모리를 할당해줍니다// 
 	
-	deliverySystem[x][y].building=nBuilding;
+	deliverySystem[x][y].building=nBuilding; 
 	deliverySystem[x][y].room = nRoom;
 	strcpy(deliverySystem[x][y].passwd,passwd);
 	strcpy(deliverySystem[x][y].context, msg);
@@ -128,27 +144,53 @@ int str_pushToStorage(int x, int y, int nBuilding, int nRoom, char msg[MAX_MSG_S
 	
 	if(deliverySystem[x][y].cnt!=1)
 	{
-		return -1;
+		return -1; //택배가 보관함에 안들어갔으면 -1을 return 합니다// 
 	}
 	
-	else 
-	return 0;
+	else //성공하면  
+	storedCnt++; //택배수 ++ 
+	str_backupSystem(STORAGE_FILEPATH);//텍스트파일에도 써줍니다// 
+	
+	return 0; 
 
 }
 
-//free the memory of the deliverySystem 
-void str_freeSystem(void) {
-	
-	int i;
-	
-	for(i=0;i<4;i++)
-	{
 
-		free(deliverySystem[i]);
-			
-	}
+
+// ------- API function for main.c file ---------------
+
+//backup the delivery system context to the file system
+//char* filepath : filepath and name to write
+//return : 0 - backup was successfully done, -1 - failed to backup
+int str_backupSystem(char* filepath) { 
+	
+	
+	FILE *fp;
+	int i,j;
+	fp=fopen(filepath,"w"); //쓰기모드로 열어줍니다(새로 써줍니다)// 
+	
+	fprintf(fp,"%d %d\n%s\n", systemSize[0], systemSize[1], masterPassword); 
 		
-		free(deliverySystem);
+	for (i=0;i<systemSize[0];i++)
+	{
+		for(j=0;j<systemSize[1];j++)
+		if (deliverySystem[i][j].cnt==1) //deliverySystem[i][j].cnt==1인 경우만 새로 써줍니다// 
+		{	
+			fprintf(fp,"%i %i %i %i %s %s\n",i,j,deliverySystem[i][j].building, deliverySystem[i][j].room, deliverySystem[i][j].passwd, deliverySystem[i][j].context);		
+		}
+	}
+
+	fclose(fp); //닫아주는걸 잊지맙니다// 
+	
+	
+	if(fp==NULL)
+	{
+		return -1;
+	} 
+	
+	else 
+	
+	return 0;  
 	
 }
 
@@ -170,39 +212,88 @@ int str_checkStorage(int x, int y) {
 }
 
 
-//get password input and check if it is correct for the cell (x,y)
-//int x, int y : cell for password check
-//return : 0 - password is matching, -1 - password is not matching
-static int inputPasswd(int x, int y) {
-	
-	char input_passwd[PASSWD_LEN+1];
-	printf("input password for (%i,%i) storage : \n",x,y);
-	scanf("%s",input_passwd);
-	
-	if (strcmp(input_passwd,deliverySystem[x][y].passwd)==0) //password is matching 
-	{
-		return 0;
-	}
-	
-	else //not matching 
-	
-	return -1;
-	
-}
+
 
 //initialize the storage
 //set all the member variable as an initial value
 //int x, int y : cell coordinate to be initialized
-static void initStorage(int x, int y) {
+
+static void initStorage(int x, int y) { //extractStorage 함수를 만들때 사용할 것입니다 1// 
 	
 	
-	deliverySystem[x][y].building=NULL;
+	deliverySystem[x][y].building=NULL; 
 	deliverySystem[x][y].room=NULL;
 	deliverySystem[x][y].cnt = 0;
 	deliverySystem[x][y].passwd[PASSWD_LEN+1]=NULL;
 	
 	free(deliverySystem[x][y].context); ///free memory	
 	
+}
+//get password input and check if it is correct for the cell (x,y)
+//int x, int y : cell for password check
+//return : 0 - password is matching, -1 - password is not matching
+
+static int inputPasswd(int x, int y) { //extractStorage 함수를 만들때 사용할 것입니다 2// 
+	
+	char input_passwd[PASSWD_LEN+1]; //문자열을 받습니다// 
+	printf("input password for (%i,%i) storage : ",x,y);
+	scanf("%s",input_passwd); 
+	
+	if (strcmp(input_passwd,deliverySystem[x][y].passwd)==0) //비밀번호가 맞으면// 
+	{
+		return 0; 
+	}
+	
+	else if (strcmp(input_passwd,masterPassword)==0) //비밀번호가 마스터비밀번호면// 
+	{
+		return 0;
+	}
+	
+	else //틀리면//
+	 
+	return -1;
+	
+}
+
+
+//extract the package context with password checking
+//after password checking, then put the msg string on the screen and re-initialize the storage
+//int x, int y : coordinate of the cell to extract
+//return : 0 - successfully extracted, -1 = failed to extract
+int str_extractStorage(int x, int y) {
+	
+	
+	int check_passwd=inputPasswd(x,y); //아까만든 비밀번호 확인해주는 함수를 만듭니다// 
+	
+	if (check_passwd!=0) //틀릴 때// 
+	{
+		printf("password is wrong!!\n");
+		return -1;
+	}
+	
+	else if (check_passwd==0) //맞거나 마스터비밀번호 일 때// 
+	{
+		
+			
+		printf("---------->extracting the storage (%i,%i)...",x,y);
+		printf("\n------------------------------------------------\n");
+		printf("<<<<<<<<<<<<<<< : %s>>>>>>>>>>>>>>>>>>>",deliverySystem[x][y].context);
+		printf("\n------------------------------------------------\n");
+		
+		initStorage(x,y); //보관함을 비워줌// 
+		str_backupSystem(STORAGE_FILEPATH); //텍스트파일에도 써줌// 
+		storedCnt--; //보관된 택배 -1// 
+					
+	}
+	
+	if (deliverySystem[x][y].cnt!=0) //보관함 안비워졌으면// 
+	{
+		return -1;
+	}
+	
+	else
+	return 0; 
+
 }
 
 //find my package from the storage
@@ -215,50 +306,32 @@ int str_findStorage(int nBuilding, int nRoom) {
 	int cnt;
 	int x,y;
 	
-		for (x=0;x<4;x++)
+		for (x=0;x<systemSize[0];x++)
 		{
-			for(y=0;y<6;y++)
+			for(y=0;y<systemSize[1];y++) //모든 보관함을 다 확인합니다// 
 			
-			if(deliverySystem[x][y].building==nBuilding && deliverySystem[x][y].room==nRoom)
-			printf("Find a package in (%i,%i)\n",x,y);
+			if(deliverySystem[x][y].building==nBuilding && deliverySystem[x][y].room==nRoom) //찾습니다// 
+			printf("Find a package in (%i,%i)\n",x,y); //알려줍니다// 
 			cnt++;
 		}
 		
-	return cnt;
+	return cnt; //내 택배가  없으면 cnt=0// 
 }
 
 
-
-
-//extract the package context with password checking
-//after password checking, then put the msg string on the screen and re-initialize the storage
-//int x, int y : coordinate of the cell to extract
-//return : 0 - successfully extracted, -1 = failed to extract
-int str_extractStorage(int x, int y) {
+//free the memory of the deliverySystem 
+void str_freeSystem(void) {
 	
+	int i;
 	
-	int check_passwd=inputPasswd(x,y);
-	
-	if (check_passwd!=0)
+	for(i=0;i<systemSize[0];i++)
 	{
-		printf("password is wrong!!\n");
-		return -1;
-	}
-	
-	else if (check_passwd==0)
-	{
-		
+
+		free(deliverySystem[i]);
 			
-		printf("---------->extracting the storage (%i,%i)...",x,y);
-		printf("\n------------------------------------------------\n");
-		printf("<<<<<<<<<<<<<<< : %s>>>>>>>>>>>>>>>>>>>",deliverySystem[x][y].context);
-		printf("\n------------------------------------------------\n");
-		
-		initStorage(x,y);
-				
-		return 0;
 	}
-	
+		
+		free(deliverySystem); //마지막에 deliverySystem 에 할당해줬던 메모리를 해제해줍니다// 
 	
 }
 
@@ -310,7 +383,7 @@ int main(int argc, char *argv[]) {
 	}
 	
 	printf("------- Unmanned Delivery Storage System START -------\n");
-	
+		
 	do
 	{
         //2. menu printing
